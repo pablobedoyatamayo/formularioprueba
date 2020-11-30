ea4a61c9abc1f8f7daf43f8786229fd4d7adfad2
pablobedoyatamayo <70729045+pablobedoyatamayo@users.noreply.github.com>
2020-11-30 11:41:33 -0500

    Add files via upload

---
+++ formulario/index.php
@@ -0,0 +1,81 @@
+<?php
+	$servidor="localhost";
+	$usuario="root";
+	$clave="";
+	$baseDeDatos="formulario";
+
+	$enlace = mysqli_connect($servidor, $usuario, $clave, $baseDeDatos);
+
+	if(!$enlace){
+		echo"Error en la conexion con el servidor";
+	}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"> 
+        <title>Formulario De Registro</title>
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+	<div class="contenedor">
+		<form action="#" class="formulario" id="formulario" name="formulario" method="POST">
+			<div class="contenedor-inputs">
+				<input type="text" name="nombre" placeholder="Nombre">
+				<input type="text" name="correo" placeholder="Correo">
+
+				<div class="sexo">
+					<input type="radio" name="sexo" id="hombre" value="hombre">
+					<label class="label-radio hombre" for="hombre">Hombre</label>
+
+					<input type="radio" name="sexo" id="mujer" value="mujer">
+					<label class="label-radio mujer" for="mujer">Mujer</label>
+				</div>
+
+				<div class="terminos">
+					<input type="checkbox" name="terminos" id="terminos">
+					<label for="terminos">Acepto Terminos y Condiciones</label>
+				</div>
+
+				<ul class="error" id="error"></ul>
+			</div>
+
+			<input type="submit" class="btn" name="registrarse" value="Registrate">
+		</form>
+		<div class="tabla">
+			<table>
+				<tr>
+					<th>ID</th>
+					<th>Nombre</th>
+					<th>Correo</th>
+					<th>Sexo</th>
+				</tr>
+					
+						
+				
+				
+			</table>
+		</div>
+	</div>
+	<script src="formulario.js"></script>
+</body>
+</html>
+<?php
+	if(isset($_POST['registrarse'])){
+		$nombre =$_POST["nombre"];
+		$correo=$_POST["correo"];
+		$sexo=$_POST["sexo"];
+		$id= rand(1,99);
+
+		$insertarDatos = "INSERT INTO datos VALUES('$nombre',
+													'$correo',
+													'$sexo',
+													'$id')";
+
+		$ejecutarInsertar = mysqli_query($enlace, $insertarDatos);
+
+		if(!$ejecutarInsertar){
+			echo"Error En la linea de sql";
+		}
+	}
+?>
